33547c7b7c8d18d147825822c5c2b0593c787e5f
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-06-06 23:47:25 +0300

    Protoyped handle_input

---
+++ graphics.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   graphics.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/06 23:35:03 by irychkov          #+#    #+#             */
+/*   Updated: 2024/06/06 23:41:23 by irychkov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+	//Escape, obstacles, collectible validation. Render map.
+void handle_input(t_game *game, int key) {
+	if (key == MLX_KEY_W)
+		game->player_y -= 1;
+	else if (key == MLX_KEY_S)
+		game->player_y += 1;
+	else if (key == MLX_KEY_A)
+		game->player_x -= 1;
+	else if (key == MLX_KEY_D)
+		game->player_x += 1;
+	render_game(game);
+}
