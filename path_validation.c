d363093be0c47ba5cb87dea84e6af268724adddb
Iurii Rychkov <irychkov@student.hive.fi>
2024-06-12 17:22:34 +0300

    draft with map_copy

--- path_validation.c
+++ path_validation.c
@@ -6,92 +6,68 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 13:28:09 by irychkov          #+#    #+#             */
-/*   Updated: 2024/06/11 14:53:15 by irychkov         ###   ########.fr       */
+/*   Updated: 2024/06/12 17:06:31 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	init_queue(t_queue *q, int capacity)
+#include "so_long.h"
+
+static char	**create_temp_map(t_game *game)
 {
-	q->capacity = capacity;
-	q->items = malloc(sizeof(t_position) * capacity);
-	q->front = 0;
-	q->rear = 0;
-	q->size = 0;
+	size_t		i;
+	char	**temp_map;
+
+	temp_map = malloc(sizeof(char *) * (game->map_height + 1));
+	if (!temp_map)
+		return (NULL);
+	i = 0;
+	while (i < game->map_height)
+	{
+		temp_map[i] = ft_strdup(game->map[i]);
+		if (!temp_map[i])
+		{
+			for (size_t j = 0; j < i; j++)
+				free(temp_map[j]);
+			free(temp_map);
+			return (NULL);
+		}
+	}
+	temp_map[game->map_height] = NULL;
+	return (temp_map);
 }
 
-int	enqueue(t_queue *q, t_position item)
+static int	explore_path(t_game *game,char **temp_map, t_position position)
 {
-	if (q->size >= q->capacity)
+	static int	collect = 0;
+	static int	exit_found = 0;
+
+	if (temp_map[position.y][position.x] == '1')
 		return (0);
-	q->items[q->rear] = item;
-	q->rear = (q->rear + 1) % q->capacity;
-	q->size++;
-	return (1);
-}
-
-t_position	dequeue(t_queue *q)
-{
-	t_position	item;
-
-	item = {0, 0};
-	if (q->size == 0)
-		return (item);
-	item = q->items[q->front];
-	q->front = (q->front + 1) % q->capacity;
-	q->size--;
-	return (item);
-}
-
-int	is_queue_empty(t_queue *q)
-{
-	if (q->size == 0)
+	else if (temp_map[position.y][position.x] == 'C')
+		collect++;
+	else if (temp_map[position.y][position.x] == 'E')
+		exit_found = 1;
+	temp_map[position.y][position.x] = '1';
+	explore_path(game, temp_map, (t_position){position.x + 1, position.y});
+	explore_path(game, temp_map, (t_position){position.x - 1, position.y});
+	explore_path(game, temp_map, (t_position){position.x, position.y + 1});
+	explore_path(game, temp_map, (t_position){position.x, position.y - 1});
+	if (collect == game->collectibles && exit_found)
 		return (1);
 	return (0);
 }
 
-int	bfs_validate_path(t_game *game, t_position start)
+int	validate_path(t_game *game, t_position position)
 {
-	t_queue		queue;
-	t_position	current;
-	int			collected_count;
-	int			marked[game->map_height][game->map_width];
-	int			directions[4][2];
+	int		result;
+	char	**temp;
 
-	collected_count = 0;
-	init_queue(&queue, game->map_width * game->map_height);
-	ft_memset(marked, 0, sizeof(marked));
-	enqueue(&queue, start);
-	marked[start.y][start.x] = 1;
-	while (!is_queue_empty(&queue))
-	{
-		current = dequeue(&queue);
-		directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-		int i = 0;
-		
-		while (i < 4)
-		{
-			int new_x = current.x + directions[i][0];
-			int new_y = current.y + directions[i][1];
-
-			if (new_x >= 0 && new_x < game->map_width && new_y >= 0
-				&& new_y < game->map_height && !marked[new_y][new_x]
-				&& game->map[new_y][new_x] != '1')
-			{
-				marked[new_y][new_x] = 1;
-				if (game->map[new_y][new_x] == 'C')
-					collected_count++;
-				if (game->map[new_y][new_x] == 'E' &&
-					collected_count == game->collectibles)
-				{
-					free(queue.items);
-					return (1);
-				}
-				enqueue(&queue, (t_position){new_x, new_y});
-			}
-			i++;
-		}
-	}
-
-	free(queue.items);
-	return (0);
+	temp = create_temp_map(game);
+	if (!temp)
+		return (0);	//not sure what type of errors
+	result = explore_path(game, temp, position);
+	for (size_t i = 0; i < game->map_height; i++)
+		free(temp[i]);
+	free(temp);
+	return (result);
 }
