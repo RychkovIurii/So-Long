1ab86c1347c2b977f0486d96de12f60b1761b6f4
Iurii Rychkov <irychkov@student.hive.fi>
2024-06-12 17:19:36 +0300

    Add cleanup_game and map_valid to main

--- main.c
+++ main.c
@@ -6,21 +6,42 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 11:36:12 by irychkov          #+#    #+#             */
-/*   Updated: 2024/06/11 08:48:20 by irychkov         ###   ########.fr       */
+/*   Updated: 2024/06/12 17:12:27 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+void cleanup_game(t_game *game) {
+	if (game->mlx) {
+		if (game->wall) mlx_delete_image(game->mlx, game->wall);
+		if (game->floor) mlx_delete_image(game->mlx, game->floor);
+		if (game->collectible) mlx_delete_image(game->mlx, game->collectible);
+		if (game->exit_img) mlx_delete_image(game->mlx, game->exit_img);
+		if (game->player) mlx_delete_image(game->mlx, game->player);
+
+		for (size_t i = 0; i < game->map_height; i++)
+			free(game->map[i]);
+		free(game->map);
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	t_game	game;
+	ft_memset(&game, 0, sizeof(t_game));
 
 	if (argc != 2)
 	{
 		printf("Usage: %s <map_file>\n", argv[0]);
 		return (1);
 	}
+
+	if (!ft_validate_file_ext(argv[1])) {
+		fprintf(stderr, "Invalid file extension. File must end with .ber\n");
+		return 1;
+	}
+
 	game.mlx = mlx_init(960, 640, "So Long", true);
 	if (!game.mlx)
 	{
@@ -31,25 +52,38 @@ int	main(int argc, char **argv)
 	load_map(&game, argv[1]);
 	if (!game.map)
 	{
+		cleanup_game(&game);
 		mlx_terminate(game.mlx);
 		return (1);
 	}
-	game.steps = 0;
-	game.gameover = 0;
+
+	if (!ft_check_chars(&game)) {
+		fprintf(stderr, "Map contains invalid characters\n");
+		cleanup_game(&game);
+		mlx_terminate(game.mlx);
+		return 1;
+	}
 	load_images(&game);
 	parse_map(&game);
+	printf("height - %zu. width - %zu.", game.map_height, game.map_width);
+	if (!ft_validate_walls(&game))
+	{
+		cleanup_game(&game);
+		mlx_terminate(game.mlx);
+		fprintf(stderr, "Map validation failed\n");
+		return (1);
+	}
+/* 	t_position player_start = {game.player_x, game.player_y};
+	if (!validate_path(&game, player_start))
+	{
+		cleanup_game(&game);
+		mlx_terminate(game.mlx);
+		fprintf(stderr, "No valid path to collect all items and reach the exit\n");
+		return (1);
+	} */
 	mlx_key_hook(game.mlx, &my_keyhook, &game);
 	mlx_loop(game.mlx);
-	mlx_delete_image(game.mlx, game.wall);
-	mlx_delete_image(game.mlx, game.floor);
-	mlx_delete_image(game.mlx, game.collectible);
-	mlx_delete_image(game.mlx, game.exit_img);
-	mlx_delete_image(game.mlx, game.player);
-
-	for (size_t i = 0; i < game.map_height; i++)
-		free(game.map[i]);
-	free(game.map);
-
+	cleanup_game(&game);
 	mlx_terminate(game.mlx);
 	return (0);
 }
